a91d79632b878726f21788923613704f791444ab
Artist <130434238+artist0pa@users.noreply.github.com>
2024-04-19 20:41:56 +0530

    project 1

---
+++ colors.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded',function()
+{
+    const buttons=document.querySelectorAll('.button')
+    const body= document.querySelector('body')
+       buttons.forEach(function(button){
+        button.addEventListener('click',function(e){
+            body.style.backgroundColor=e.target.id
+        })
+       }) 
+    
+})
